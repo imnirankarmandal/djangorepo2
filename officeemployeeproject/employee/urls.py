09cadfded7cbc06da7d94dd3a68afe70c79e5eb4
Nirankar <niranakr.d.mandal@gmail.com>
2023-05-21 17:53:11 +0530

    second commit by Nirankar

--- officeemployeeproject/employee/urls.py
+++ officeemployeeproject/employee/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('home/', views.home_view, name='home'),
     path('emplist/', views.list_view, name='list'),
     path('add_emp/', views.add_view, name='add_emp'),
     path('remove_emp/', views.remove_view, name='remove_emp'),
